8b339175f02efd58f6189e5f09389100178996cb
chauhoangtan <chauhoangtan6937@gmail.com>
2025-07-31 16:49:05 +0700

    add eureka server

--- server/eureka-service/src/main/java/com/example/eureka/EurekaServiceApplication.java
+++ server/eureka-service/src/main/java/com/example/eureka/EurekaServiceApplication.java
@@ -11,6 +11,7 @@ public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
+		System.out.println("Eureka Server is running...");
 	}
 
 }
